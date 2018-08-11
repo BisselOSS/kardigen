71eafb6a6d15e45d300193deba1c08d1fe1de1b3
Sebastian Sellmair <sebastiansellmair@gmail.com>
2018-08-11 18:49:49 +0200

    Draft of BindingImpl

--- kardigen-compiler/src/main/java/com/github/bissel/kardigen/compiler/BindingImpl.kt
+++ kardigen-compiler/src/main/java/com/github/bissel/kardigen/compiler/BindingImpl.kt
@@ -1,9 +1,13 @@
 package com.github.bissel.kardigen.compiler
 
+import com.github.bissel.kardigen.annotation.KodeinBind
 import com.github.bissel.kardigen.annotation.KodeinSingleton
+import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.asTypeName
 import javax.annotation.processing.ProcessingEnvironment
 import javax.lang.model.element.Element
+import javax.lang.model.element.ExecutableElement
 
 class BindingImpl(private val environment: ProcessingEnvironment,
                   override val element: Element) : Binding {
@@ -16,13 +20,23 @@ class BindingImpl(private val environment: ProcessingEnvironment,
     }
 
     private fun renderProvider(target: FunSpec.Builder) {
-        target.addCode("""
-            bind<
-        """.trimIndent())
+        target.addCode(
+            """
+            bind<%T>() with provider {
+                %T($injectStatement)
+            }
+            """.trimIndent(),
+            bindingClassName, realClassName)
     }
 
     private fun renderSingleton(target: FunSpec.Builder) {
-
+        target.addCode(
+            """
+            bind<%T>() with singleton {
+                %T($injectStatement)
+            }
+            """.trimIndent(),
+            bindingClassName, realClassName)
     }
 
 
@@ -35,9 +49,29 @@ class BindingImpl(private val environment: ProcessingEnvironment,
         singletonAnnotation != null
     }
 
-    private val bindingType by lazy {
-        // val element.getAnnotation(KodeinBind::class.java)
+    private val bindingClassName: ClassName by lazy {
+        val bindAnnotation: KodeinBind? = element.getAnnotation(KodeinBind::class.java)
+        when {
+            bindAnnotation != null -> bindAnnotation.value.java.asTypeName() as ClassName
+            else -> realClassName
+        }
     }
 
+
+    private val realClassName: ClassName by lazy {
+        classElement.asType().asTypeName() as ClassName
+    }
+
+
+    private val parameterCount by lazy {
+        val executableElement = element as ExecutableElement
+        executableElement.parameters.size
+    }
+
+    private val injectStatement by lazy {
+        (0 until parameterCount).asSequence()
+            .map { "instance()" }
+            .joinToString(", ")
+    }
 
 }
