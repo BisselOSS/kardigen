53be07e07518676c3d76f606d653367c7c87e658
Sebastian Sellmair <sebastiansellmair@gmail.com>
2018-08-11 17:58:06 +0200

    Idea draft

--- kardigen-compiler/src/main/java/com/github/bissel/kardigen/compiler/Processor.kt
+++ kardigen-compiler/src/main/java/com/github/bissel/kardigen/compiler/Processor.kt
@@ -3,9 +3,12 @@ package com.github.bissel.kardigen.compiler
 import com.github.bissel.kardigen.annotation.KodeinInject
 import com.github.bissel.kardigen.annotation.KodeinModule
 import com.github.bissel.kardigen.annotation.KodeinSingleton
+import com.squareup.kotlinpoet.FunSpec
 import javax.annotation.processing.AbstractProcessor
+import javax.annotation.processing.ProcessingEnvironment
 import javax.annotation.processing.RoundEnvironment
 import javax.lang.model.SourceVersion
+import javax.lang.model.element.Element
 import javax.lang.model.element.TypeElement
 
 class Processor : AbstractProcessor() {
@@ -17,8 +20,38 @@ class Processor : AbstractProcessor() {
 
     override fun getSupportedSourceVersion(): SourceVersion = SourceVersion.latest()
 
-    override fun process(p0: MutableSet<out TypeElement>?, p1: RoundEnvironment?): Boolean {
+    override fun process(set: MutableSet<out TypeElement>, environment: RoundEnvironment): Boolean {
+        val injectConstructors = environment.getElementsAnnotatedWith(KodeinInject::class.java)
+        val modules = modules(injectConstructors)
+        render(modules)
+
         return true
     }
 
+
+    private fun modules(constructors: Set<Element>): List<Module> {
+        TODO()
+    }
+
+
+    private fun render(modules: List<Module>) {
+        for (module in modules) {
+            module.render(processingEnv)
+        }
+    }
+}
+
+
+interface Module : Renderable<ProcessingEnvironment> {
+    val name: String
+    val bindings: List<Binding>
+}
+
+interface Binding : Renderable<FunSpec.Builder> {
+    val element: Element
+}
+
+
+interface Renderable<Target> {
+    fun render(target: Target)
 }
