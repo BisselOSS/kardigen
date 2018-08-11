17dee35d0e40c028e8037b6201493b16cc3ce9e1
Sebastian Sellmair <sebastiansellmair@gmail.com>
2018-08-11 18:00:43 +0200

    Idea draft

--- kardigen-compiler/src/main/java/com/github/bissel/kardigen/compiler/BindingImpl.kt
+++ kardigen-compiler/src/main/java/com/github/bissel/kardigen/compiler/BindingImpl.kt
@@ -3,9 +3,7 @@ package com.github.bissel.kardigen.compiler
 import com.squareup.kotlinpoet.FunSpec
 import javax.lang.model.element.Element
 
-class BindingImpl : Binding {
-    override val element: Element
-        get() = TODO("not implemented")
+class BindingImpl(override val element: Element) : Binding {
 
     override fun render(target: FunSpec.Builder) {
         TODO("not implemented")
